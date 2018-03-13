ec923cf45ca2c04def5a14f8b2597878fa40d23c
unknown <KarenChabonian>
2018-03-13 11:55:54 +0200

    certain changes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
-    <HelloWorld/>
+    <PostSend/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import PostSend from './components/PostSend'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  PostSend
   }
 }
 </script>
---
+++ src/components/PostSend.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="form">
+    <label>Author</label>
+    <input v-model="author" type="text">
+    <label>Text</label>
+    <input v-model="text" type="text">
+    <label>Title</label>
+    <input v-model="Title" type="text">
+    <label>Upload image</label>
+    <input class="file" type="file" id="files" name="files" multiple>
+    <button @click="sendPost()" type="submit">Send</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+  name: 'PostSend',
+  data () {
+    return {
+      author: '',
+      text: '',
+      title: '',
+      list: []
+    }
+  },
+
+  mounted() {
+    this.getPostsList()
+  },
+
+ getPostsList() {
+      this.$http.get('https://tzf.herokuapp.com/news')
+      
+      .then(response => {
+          console.log(response);
+          return response.body;
+      })
+
+      .then(body => {
+          console.log(body);
+          this.list = body;
+      })
+    },
+
+  methods: {
+    parseBody(body) {
+      const formData = new FormData();
+      for (var key in body) {
+      if (body.hasOwnProperty(key)) {
+        var element = body[key];
+        if(element) {
+          formData.append(key, element);
+          }
+        }
+      }        
+      return formData;
+    },
+
+    getAdminsList() {
+      this.$http.get('https://tzf.herokuapp.com/news')
+      
+      .then(response => {
+          console.log(response);
+          return response.body;
+      })
+
+      .then(body => {
+          console.log(body);
+          this.list = body;
+      })
+    },
+
+    sendPost() {
+      let newData = {
+        author:this.author,
+        text:this.text,
+        title: this.title,
+        image: document.querySelector('#files').files[0]
+      };
+      console.log(newData);
+      this.$http.post('https://tzf.herokuapp.com/create-news', this.parseBody(newData))
+      .then(response => {
+        response.json();
+        console.log(response);
+      })      
+    },
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+label {
+  font-size: 1.5rem;
+}
+
+input {
+  margin: 15px;
+  padding: 10px;
+}
+
+button {
+  margin-top: 10px;
+  padding: 10px;
+}
+
+.form {
+  background: #42b983;
+  width: 400px;          
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  padding: 15px;
+}
+
+.file {
+  margin: 0 auto;
+} 
+
+
+</style>
